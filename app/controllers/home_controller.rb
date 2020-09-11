f0796921232b9627f1d0ebbd513b931fc78246ca
RamachandranKulothungan <ramachandran.kulo@gmail.com>
2020-09-11 12:57:44 +0530

    rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,17 @@ class HomeController < ApplicationController
         #views/controllername/index.html.erb
         render plain:"OK"
     end
+    def index
+        #nil
+        #try to render default view
+        #views/controllername/index.html.erb
+        #render plain:"Index"
+    end
+    def contact
+        
+    end
+    def about
+        
+        
+    end
 end
