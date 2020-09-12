32b1f047f4587a5444e4f99f37c174050e2c3e59
RamachandranKulothungan <ramachandran.kulo@gmail.com>
2020-09-12 13:24:20 +0530

    ror

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,42 @@
 class TagsController < ApplicationController
-    before_action :set_tag, only: [:show, :destroy]
+    before_action :set_tag, only: [:show, :destroy, :edit, :update]
   def index
     @tags = Tag.all
+    if params[:format] == "html"
+    elsif params[:format] == "json"
+        render json: @tags
+    end
   end
+
   def show
     #@tag = Tag.find(params[:id])
     
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    p params
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to tags_path,notice: "Tag was successfully added"
+    else
+      render :new 
+    end
+  end
+  def edit
+    
+  end
+
+  def update
+
+    if @tag.update(tag_params)
+      redirect_to tags_path,notice: "Tag was successfully updated"
+    else
+      render :edit 
+    end
   end
   def destroy
     #@tag = Tag.find(params[:id])
@@ -15,6 +46,10 @@ class TagsController < ApplicationController
   private 
   def set_tag
     @tag = Tag.find(params[:id])
+    
+  end
+  def tag_params
+    params.require(:tag).permit(:name)
     
   end
 end
